3b942731a03d214e4879a5455ab6f886b0af04f2
Buster Daemon <bugsbunnygdi@pm.me>
2024-03-21 21:24:14 +0300

    Added ability for deleting files

--- internal/tui/model.go
+++ internal/tui/model.go
@@ -1,6 +1,7 @@
 package tui
 
 import (
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
@@ -11,12 +12,23 @@ import (
 	"github.com/charmbracelet/lipgloss"
 )
 
+const (
+	DELETE_STATUS_OFF = iota
+	DELETE_STATUS_ON
+)
+
+const (
+	CURRENT_DIRECTORY = "Current directory: %s"
+	CONFIRM_DELETE    = "Are you sure want to delete the %s?"
+)
+
 type Model struct {
-	files     []table.Row
-	directory string
-	table     table.Model
-	hexDeciph viewport.Model
-	style     lipgloss.Style
+	files           []table.Row
+	directory       string
+	table           table.Model
+	hexDeciph       viewport.Model
+	deleteFDirState int8
+	style           lipgloss.Style
 }
 
 func (m Model) Init() tea.Cmd {
@@ -35,11 +47,33 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 		m.table.SetWidth(ms.Width - 4)
 		m.table.SetHeight(ms.Height/2 - 4)
+		m.table.UpdateViewport()
 		m.hexDeciph.Width = ms.Width - 4
 		m.hexDeciph.Height = ms.Height/2 - 4
 
 	case tea.KeyMsg:
 		switch ms.String() {
+		case "c":
+			m.hexDeciph.SetContent("")
+		case "delete":
+			if m.deleteFDirState == DELETE_STATUS_ON {
+				err := os.Remove(
+					filepath.Join(
+						m.directory,
+						m.table.SelectedRow()[1],
+					))
+
+				if err != nil {
+					hexData = err.Error()
+					m.hexDeciph.SetContent(hexData)
+				}
+				m.table.SetRows(updateDirectory(m.directory))
+				m.table.SetCursor(0)
+				m.deleteFDirState = DELETE_STATUS_OFF
+			}
+			if m.deleteFDirState == DELETE_STATUS_OFF {
+				m.deleteFDirState = DELETE_STATUS_ON
+			}
 		case "ctrl+c", "q":
 			return m, tea.Quit
 		case "up":
@@ -51,6 +85,7 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "home":
 			m.table.GotoTop()
 		case "enter":
+			m.hexDeciph.SetContent("")
 			m.directory = filepath.Join(m.directory, m.table.SelectedRow()[1])
 
 			stat, err := os.Stat(m.directory)
@@ -68,7 +103,7 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				m.table.SetRows(m.files)
 				m.table.SetCursor(0)
 			} else {
-				hexData, err = readSomeFileData(m.directory)
+				hexData, err = readSomeFileData(m.directory, 1)
 				if err != nil {
 					hexData = err.Error()
 				}
@@ -86,6 +121,24 @@ func (m Model) View() string {
 		lipgloss.JoinVertical(
 			lipgloss.Top,
 			m.table.View(),
+
+			fmt.Sprintf(
+				func() string {
+					if m.deleteFDirState == 1 {
+						return CONFIRM_DELETE
+					}
+					return CURRENT_DIRECTORY
+				}(),
+				m.directory),
+
+			lipgloss.PlaceHorizontal(
+				lipgloss.Width(
+					m.table.View(),
+				),
+				lipgloss.Center,
+				"Decoded View:",
+				lipgloss.WithWhitespaceChars(lipgloss.BlockBorder().Bottom),
+			),
 			m.hexDeciph.View(),
 		),
 	)
@@ -99,6 +152,7 @@ func updateDirectory(directory string) []table.Row {
 		rows = append(rows, table.Row{
 			upperF.Mode().String(),
 			"..",
+			"",
 			upperF.ModTime().Local().Format("2006-01-02 15:04:05"),
 		})
 	}
@@ -109,11 +163,6 @@ func updateDirectory(directory string) []table.Row {
 	}
 
 	for _, i := range files {
-		// rows = append(rows, table.Row{
-		// 	i.Type().String(),
-		// 	i.Name(),
-		// 	i.Type().Type().String(),
-		// })
 		stat, err := os.Stat(filepath.Join(directory, i.Name()))
 		if err != nil {
 			return nil
@@ -121,6 +170,7 @@ func updateDirectory(directory string) []table.Row {
 		rows = append(rows, table.Row{
 			stat.Mode().String(),
 			i.Name(),
+			fmt.Sprintf("%d", stat.Size()),
 			stat.ModTime().Local().Format("2006-01-02 15:04:05"),
 		})
 	}
@@ -147,6 +197,10 @@ func InitialMode() Model {
 					Width: 30,
 				},
 				{
+					Title: "File size",
+					Width: 25,
+				},
+				{
 					Title: "Last Modified",
 					Width: 30,
 				},
@@ -156,16 +210,19 @@ func InitialMode() Model {
 		table.WithRows(updateDirectory(m.directory)),
 	)
 
-	m.hexDeciph = viewport.New(10, 10)
+	m.hexDeciph = viewport.New(5, 5)
+	m.deleteFDirState = 0
 
 	m.style = lipgloss.NewStyle().
 		AlignHorizontal(lipgloss.Top).
 		AlignVertical(lipgloss.Top).
 		Border(lipgloss.DoubleBorder(), true)
+
 	return m
 }
 
-func readSomeFileData(path string) (string, error) {
+func readSomeFileData(path string, mode int8) (string, error) {
+	var resString string
 	f, err := os.Open(path)
 	if err != nil {
 		return err.Error(), err
@@ -176,5 +233,19 @@ func readSomeFileData(path string) (string, error) {
 	if _, err = io.ReadFull(f, buffer[:]); err != nil {
 		return "", err
 	}
-	return string(buffer[:]), nil
+	switch mode {
+	case 1:
+		for _, bf := range buffer {
+			resString += fmt.Sprintf("%x", bf)
+
+			resString += " "
+
+		}
+	case 2:
+		resString = fmt.Sprintf("%s", buffer)
+	default:
+		resString = fmt.Sprintf("%s", buffer)
+	}
+
+	return resString, nil
 }
